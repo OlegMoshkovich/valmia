1562752da5a4a2f421a62527e2192df117a164b9
olegmoshkovich <oleg.mosh@gmail.com>
2026-03-02 09:05:39 +0100

    add the description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,17 +14,17 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "ValMia",
-  description: "ValMia - Digital Connective Tissues",
+  description: "ValMia - Connecting people",
   metadataBase: new URL("https://valmia.ch"),
   openGraph: {
     title: "ValMia",
-    description: "ValMia - Digital Connective Tissues",
+    description: "ValMia - Connecting people",
     images: [{ url: "/background_2.jpeg", width: 1080, height: 1920 }],
   },
   twitter: {
     card: "summary_large_image",
     title: "ValMia",
-    description: "ValMia - Digital Connective Tissues",
+    description: "ValMia - Connecting people",
     images: ["/background_2.jpeg"],
   },
 };
